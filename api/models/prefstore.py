61f18890963bed42fa7fe98ec88571a749c4d723
guserav <erik.zeiske@web.de>
2019-04-06 12:43:31 +0200

    Drop unused code of getting preferences 96 / 100%

--- api/models/prefstore.py
+++ api/models/prefstore.py
@@ -10,23 +10,10 @@ class PrefStoreClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def get_user_prefs(self, user):
-        r = requests.get("{}/preferences/user/{}".format(self.base_url, user))
-        assert r.status_code == 200
-        return r.json()
-
-    def set_user_prefs(self, user, prefs):
-        r = requests.patch("{}/preferences/user/{}".format(self.base_url, user), json = prefs)
-        assert r.status_code == 200
-
     def get_global_prefs(self):
         r = requests.get("{}/preferences/global/".format(self.base_url))
         assert r.status_code == 200
         return r.json()
-
-    def set_global_prefs(self, prefs):
-        r = requests.patch("{}/preferences/global/".format(self.base_url), json = prefs)
-        assert r.status_code == 200
 
 PREFSTORE_CLIENT = PrefStoreClient(CONFIG.CENTRAL_NODE_BASE_URL)
 
